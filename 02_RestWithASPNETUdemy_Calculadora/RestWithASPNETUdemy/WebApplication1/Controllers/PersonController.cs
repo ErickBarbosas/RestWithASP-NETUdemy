50441d3089af0d4f074ae90ede97ebce164e96d7
Erick <santoserick2015@hotmail.com>
2022-02-28 09:21:03 -0300

    Criando Construdor pessoa

---
+++ 02_RestWithASPNETUdemy_Calculadora/RestWithASPNETUdemy/WebApplication1/Controllers/PersonController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+       
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{operacao}/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string operacao,string firstNumber,string secondNumber)
+        {
+       
+        }
+
+        
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(
+                strNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number);
+
+            return isNumber;
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+       
+
+       
+
+       
+    }
+}
